e039f6997fce5872c85db4b5d22fb3d1d29d0fe1
rahat664 <rahatkabir20@gmail.com>
2022-03-16 06:51:44 +0600

    feat: Utills functions added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
 import { AppModule } from './app.module';
+import { join } from 'path';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use('/files', express.static(join(process.cwd(), 'files')));
   await app.listen(3000);
 }
 bootstrap();
