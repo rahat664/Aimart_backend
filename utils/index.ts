4965bf823ca3b332d0ce9145b2c8929969ebb8e1
rahat664 <rahatkabir20@gmail.com>
2022-03-17 06:48:17 +0600

    feat: Utils function added

---
+++ utils/index.ts
@@ -0,0 +1,7 @@
+export function modifyFileName(req, file, cb) {
+    const randomName = Array(32)
+        .fill(null)
+        .map(() => Math.round(Math.random() * 16).toString(16))
+        .join('');
+    return cb(null, `${randomName}${file.originalname}`);
+}
